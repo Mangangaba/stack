f4dacb0c46fc5ff78d155b6f3793dc91949fd997
Igor Eduardo <76757796+gu-lak@users.noreply.github.com>
2022-04-29 17:02:18 -0300

    v0.7.10
    Fixed: Google Analytics debugging.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,3 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-LR24J2E0LT');
-
 function liveViews(response) {
     document.getElementById('visits').innerText = response.value;
 }
