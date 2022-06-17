81f59745f58cea64ee0a02b8deff2719a303f95d
Igor Eduardo Gulak <76757796+gu-lak@users.noreply.github.com>
2022-06-17 13:24:59 -0300

    [v1.0]

--- src/js/main.js
+++ src/js/main.js
@@ -58,9 +58,10 @@ const sr = ScrollReveal({
 
 sr.reveal(`.profile__border`)
 sr.reveal(`.profile__name`, {delay: 500})
-sr.reveal(`.profile__profession`, {delay: 600})
+sr.reveal(`.profile__theme`, {delay: 600})
 sr.reveal(`.profile__social`, {delay: 700})
 sr.reveal(`.profile__info-group`, {interval: 100, delay: 700})
 sr.reveal(`.profile__buttons`, {delay: 800})
+sr.reveal(`.footer__copy`, {delay: 350})
 sr.reveal(`.filters__content`, {delay: 900})
 sr.reveal(`.filters`, {delay: 1000})
